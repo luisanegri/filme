9fef6f58c8348d4cd4cd78d5447ff711e7684616
unknown <luisaanegri@gmail.com>
2018-06-06 17:11:55 +0100

    changed function name/modified function output

--- main.js
+++ main.js
@@ -15,36 +15,35 @@ function getFilms(searchText) {
 
       //data stored on the films variable
       let films = response.data.results;
-      let output = '';
+      let output = [];
       $.each(films, (index, film) => {
-        output += `
+        output.push( `
         <div class="col-md-3">
           <div class="well text-center">
             <img src="https://image.tmdb.org/t/p/w185/${film.poster_path}">
             <h5>${film.title}</h5>
-            <a onclick="filmSelected('${film.id}')" class="btn btn-primary" href="#">Film Details</a>
+            <a onclick="switchPage('${film.id}')" class="btn btn-primary" href="#">Film Details</a>
           </div>
         </div>
-      `;
+      `);
       });
-      $('#films').html(output);
+      $('#films').html(output.join(""));
     })
     .catch((err) => {
       console.log(err);
     });
 }
 //pass data to one page to another through session storage
-function filmSelected(id) {
+function switchPage(id) {
   sessionStorage.setItem('filmId', id);
   window.location = 'film.html';
   return false;
 }
 
-function getFilmDetails() {
+function filmDetails() {
   let filmId = sessionStorage.getItem('filmId');
   axios.get('https://api.themoviedb.org/3/movie/' + filmId + '?api_key=481bce5538131467b4d3508eda2d7e05&language=en-US')
     .then((response) => {
-      console.log(response);
       //store data
       let film = response.data;
       let output = `
@@ -55,7 +54,7 @@ function getFilmDetails() {
         <div class="col-md-8  desc">
           <h2>${film.title}</h2>
           <ul class="list-group">
-            <li class="list-group-item"><strong>Genre:</strong> ${film.genres.name}</li>
+            <li class="list-group-item"><strong>Genre:</strong> ${film.genres[0].name}</li>
             <li class="list-group-item"><strong>Released:</strong> ${film.release_date}</li>
             <li class="list-group-item"><strong>Rated:</strong> ${film.vote_average}</li>
             <li class="list-group-item"><a href="${film.homepage}" target="_blank"> Website</a></li>
@@ -77,58 +76,56 @@ function getFilmDetails() {
     });
 }
 
-function getRecommendations() {
+function recommendations() {
   let filmId = sessionStorage.getItem('filmId');
   axios.get('https://api.themoviedb.org/3/movie/' + filmId + '/recommendations?api_key=481bce5538131467b4d3508eda2d7e05&language=en-US&page=1')
     .then((response) => {
-      console.log(response);
       let recommend = response.data.results;
-      let output = '';
-      $.each(recommend, (index, film) => {
-        if (index > 5) {
+      let output = [];
+      $.each(recommend, (index, film) => { //use for loop
+        if (index > 5) { //do const
           return false;
         }
-        output += `
+        output.push( `
        <div class="col-md-2">
          <div class="well text-center">
-           <a onclick="filmSelected('${film.id}')" href="#"><img src="https://image.tmdb.org/t/p/w185/${film.poster_path}"></a>
-           <a onclick="filmSelected('${film.id}')" href="#"><h5>${film.title}</h5></a>
+           <a onclick="switchPage('${film.id}')" href="#"><img src="https://image.tmdb.org/t/p/w185/${film.poster_path}"></a>
+           <a onclick="switchPage('${film.id}')" href="#"><h5>${film.title}</h5></a>
          </div>
        </div>
        <hr>
-       `;
+       `);
       });
-      $('#recommend').html(output);
-      getFilmDetails();
+      $('#recommend').html(output.join(""));
+      filmDetails();
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-function getPopFilms() {
+function popularFilms() {
   axios.get('https://api.themoviedb.org/3/discover/movie?api_key=481bce5538131467b4d3508eda2d7e05&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
     .then((response) => {
-      console.log(response);
-      let popFilms = response.data.results;
-      let popOutput = '';
-      $.each(popFilms, (index, film) => {
+      let popular = response.data.results;
+      let output = [];
+      $.each(popular, (index, film) => {
         if (index > 5) {
           return false;
         }
-        popOutput += `
-<div class="col-md-2 col-sm-4 col-xs-6">
-      <div class="well text-center">
-        <a onclick="filmSelected('${film.id}')" href="#"><img src="https://image.tmdb.org/t/p/w185/${film.poster_path}" class="pop-img"></a>
-        <a onclick="filmSelected('${film.id}')" href="#"><h5>${film.title}</h5></a>
+        output.push(`
+          <div class="col-md-2 col-sm-4 col-xs-6">
+            <div class="well text-center">
+        <a onclick="switchPage('${film.id}')" href="#"><img src="https://image.tmdb.org/t/p/w185/${film.poster_path}" class="pop-img"></a>
+        <a onclick="switchPage('${film.id}')" href="#"><h5>${film.title}</h5></a>
       </div>
     </div>
-    `;
+    `);
       });
-      $('#popular-films').html(popOutput);
-      getFilmDetails();
+      $('#popular-films').html(output.join(""));
+      filmDetails();
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     });
 }
